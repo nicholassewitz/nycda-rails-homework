d0beb9b7bfaa1ef0049ace7f01b57fdcbc5e7b90
Nicholas Sewitz <nicholassewitz@Nicholass-MacBook-Pro.local>
2016-09-25 13:31:14 -0400

    basically done need to test

---
+++ homework-7/devise/app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action(:find_user, only: [:show, :edit, :update, :destroy])
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @tweet = Tweet.create(tweet_params.merge(user: current_user))
+    @user = current_user.users.create(user_params)
+    redirect_to_user_if_valid('You successfully created a user')
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to_user_if_valid('You successfully created a user')
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to(users_path)
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params[:user].permit(:name,:headline,:profile,:email)
+  end
+
+  def redirect_to_user_if_valid(notice)
+    redirect_to(@user, notice: notice) if @user.valid?
+  end
+
+  def redirect_to_user(notice)
+    redirect_to(@user, notice: notice)
+  end
+
+end
